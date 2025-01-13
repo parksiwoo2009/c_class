6a99fec66e68b42e81f8b2b0e975c86c164de018
Admin <Admin@F-3>
2025-01-13 16:46:24 +0900

    day4

---
+++ c_class/자료형의 변환.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+	//자동 형변환
+	double change_number = 10;
+	printf("%f\n", change_number);
+
+	int change_number2 = 5.11223;
+	printf("%d\n", change_number2);
+
+	printf("%d\n", (int)1.1234);
+	printf("%d\n", (short)1.1234);
+	printf("%f\n", (double)22);
+	printf("%f\n", (float)22);
+
+	float f = 3.14f;
+	printf("%f\n", f);
+	return 0;
+}  
+	
